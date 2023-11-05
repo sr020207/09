72d242f6b840d2477233d86b5b26ec0e4c9ec1f2
sr020207 <144992835+sr020207@users.noreply.github.com>
2023-11-05 23:39:19 +0900

    005

--- main.c
+++ main.c
@@ -1,29 +1,46 @@
 #include <stdio.h>
-#define SIZE 4
-void square_array(int a[], int size)
+#define ROWS 3
+#define COLS 3
+
+void addMatrix(int A[][COLS], int B[][COLS], int C[][COLS])
 {
-	int i;
-	for (i=0; i<size; i++)
-	a[i] = a[i] * a[i];
+	int i, j;
+    for (i = 0; i < ROWS; i++) {
+        for (j = 0; j < COLS; j++) {
+            C[i][j] = A[i][j] + B[i][j];
+        }
+    }
 }
 
-
-void print_array(int a[], int size)
+void printMatrix(int A[][COLS])
 {
-	int i;
-	for (i=0; i<size; i++)
-	printf("%3d", a[i]);
-	printf("\n");
+	int i, j;
+    for (i = 0; i < ROWS; i++) {
+        for (j = 0; j < COLS; j++) {
+            printf("%3d ", A[i][j]);
+        }
+        printf("\n");
+    }
 }
 
 int main(void)
 {
-	int list[SIZE] = {1, 2, 3, 4};
+    int A[ROWS][COLS] = {
+        {2, 3, 0},
+        {8, 9, 1},
+        {7, 0, 5}
+    };
 
-	print_array(list, SIZE);
-	square_array(list, SIZE);
-	print_array(list, SIZE);
-	
-	system("PAUSE");
-	return 0;
+    int B[ROWS][COLS] = {
+        {1, 0, 0},
+        {0, 1, 0},
+        {0, 0, 1}
+    };
+
+    int C[ROWS][COLS]; // µ¡¼À °á°ú Çà·Ä
+
+    addMatrix(A, B, C);
+    printMatrix(C);
+
+    return 0;
 }
